01ca84d2a5338fdef2613a7e2333a2d8bb24e62a
Jaqueline Romero <jaqelineromto@gmail.com>
2024-06-12 15:52:57 +0200

    añadir usuario alumno

--- src/main/java/mipk/LoginAuthenticator.java
+++ src/main/java/mipk/LoginAuthenticator.java
@@ -7,15 +7,16 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
-
 /**
  * Servlet implementation class LoginAuthenticator
  */
 public class LoginAuthenticator extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	private String usuvalido = "user1";
-	private String pwdvalida = "jaquito";
-       
+	private String usuvalido1 = "user1";
+	private String pwdvalida1 = "jaquito";
+	private String usuvalido2 = "alumno";
+	private String pwdvalida2 = "1234";
+	
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -33,24 +34,21 @@ public class LoginAuthenticator extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		beanDB db = new beanDB();
 		HttpSession session = request.getSession();
-		String usuario=request.getParameter("usuario");
-		String pass=request.getParameter("pass");
-		if (usuario == null) usuario="";
-		if (pass == null) pass="";
-		boolean ok=false;
+		String usuario = request.getParameter("usuario");
+		String pass = request.getParameter("pass");
+		if (usuario == null) usuario = "";
+		if (pass == null) pass = "";
+		boolean ok = false;
 		
-		if(usuario.equals(usuvalido) && pass.equals(pwdvalida)) {
-			session.setAttribute("attributo2",usuario);
-			session.setAttribute("attributo1","1");
-			ok=true;
+		if ((usuario.equals(usuvalido1) && pass.equals(pwdvalida1)) || 
+			(usuario.equals(usuvalido2) && pass.equals(pwdvalida2))) {
+			session.setAttribute("attributo2", usuario);
+			session.setAttribute("attributo1", "1");
+			ok = true;
 		}
 				
 		if (ok) response.sendRedirect("bienvenido.jsp");
 		else response.sendRedirect("index.jsp");
-
 	}
-
-
 }
